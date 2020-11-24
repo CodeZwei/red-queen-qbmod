c2a4ba8deabf8b43425acfe5797ba8e3db5c1f11
CodeZwei <brandonzwei@gmail.com>
2020-11-24 13:00:20 -0800

    Added RedQueen Avatar. Cannot actually override Avatar

--- mod/mod.js
+++ mod/mod.js
@@ -3,6 +3,8 @@
   mod.name = "Red Queen";
 
   mod.loadImage("Queen-Body-Default", "assets/Queen-Body-RedHair.png");
+  // TODO: Cannot override Avatar images
+  mod.loadImage("QueenAvatar", "assets/QueenAvatar-RedHair.png")
 
   mod.init = () => {
     // Add setup code here
